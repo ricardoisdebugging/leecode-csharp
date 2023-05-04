95418421a4321f191ecb46b9738750fd4dd2a309
Ricardo <296060440@qq.com>
2023-05-04 20:47:20 +0800

    initialize project and submit 0001-two-sum

---
+++ topics/LeeCode/0001-two-sum/TwoSumImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0001_two_sum
+{
+    public class TwoSumImpl
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var numMap = new Dictionary<int, int>();
+
+            for (int itemIndex = 0; itemIndex < nums.Length; itemIndex++)
+            {
+                var item = nums[itemIndex];
+                var remainValue = target - item;
+
+                if (numMap.ContainsKey(remainValue))
+                    return new int[] { itemIndex, numMap[remainValue] };
+                else
+                    numMap[item] = itemIndex;
+            }
+
+            return new int[] { 0 , 0 };
+        }
+    }
+}
---
+++ topics/LeeCodeTest/0001-two-sum/TwoSumTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0001_two_sum
+{
+    [TestClass]
+    public class TwoSumTest
+    {
+        [TestMethod]
+        [DataRow(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        [DataRow(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [DataRow(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        public void TwoSum(int[] nums, int target, int[] expectedValues)
+        {
+            var realValues = TwoSumImpl.TwoSum(nums, target);
+            Array.Sort(realValues);
+            CollectionAssert.AreEqual(expectedValues, realValues);
+        }
+    }
+}
