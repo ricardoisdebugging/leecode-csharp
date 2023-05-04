de3713583f760b76d670213dbf6182b0fed8da01
Ricardo <296060440@qq.com>
2023-05-04 21:24:57 +0800

    submit 0011_container_with_most_water

---
+++ topics/LeeCode/0011-container-with-most-water/ContainerWithMostWaterImpl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0011_container_with_most_water
+{
+    public class ContainerWithMostWaterImpl
+    {
+        public static int MaxArea(int[] height)
+        {
+            var maxArea = 0;
+
+            var leftIndex = 0;
+            var rightIndex = height.Length - 1;
+
+            while (leftIndex < rightIndex)
+            {
+                if (height[leftIndex] <= height[rightIndex])
+                {
+                    maxArea = Math.Max(maxArea, (rightIndex - leftIndex) * height[leftIndex]);
+
+                    leftIndex++;
+                    continue;
+                }
+
+                if (height[leftIndex] > height[rightIndex])
+                {
+                    maxArea = Math.Max(maxArea, (rightIndex - leftIndex) * height[rightIndex]);
+
+                    rightIndex--;
+                    continue;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
---
+++ topics/LeeCodeTest/0011-container-with-most-water/ContainerWithMostWaterImplTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0011_container_with_most_water;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0011_container_with_most_water
+{
+    [TestClass()]
+    public class ContainerWithMostWaterTest
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [DataRow(new int[] { 1, 1 }, 1)]
+        public void MaxAreaTest(int[] height, int expectedArea)
+        {
+            var realArea = ContainerWithMostWaterImpl.MaxArea(height);
+            Assert.AreEqual(expectedArea, realArea);
+        }
+    }
+}
