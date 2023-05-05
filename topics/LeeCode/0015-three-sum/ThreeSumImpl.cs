80cf46a958962710077269745bcd5fd36cc515d8
Ricardo <296060440@qq.com>
2023-05-05 20:33:17 +0800

    submit 0015_three_sum: two pointers with bad performance

---
+++ topics/LeeCode/0015-three-sum/ThreeSumImpl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0015_three_sum
+{
+    public class ThreeSumImpl
+    {
+        public static IList<IList<int>> ThreeSumWithTwoPointers(int[] nums)
+        {
+            Array.Sort(nums);
+
+            IList<IList<int>> results = new List<IList<int>>();
+            HashSet<string> uniqueResults = new HashSet<string>();
+
+            for (int minItemIndex = 0; minItemIndex < nums.Length - 2; minItemIndex++)
+            {
+                var leftIndex = minItemIndex + 1;
+                var rightIndex = nums.Length - 1;
+
+                while (leftIndex < rightIndex)
+                {
+                    if (nums[minItemIndex] + nums[leftIndex] + nums[rightIndex] == 0)
+                    {
+                        var tempComb = new List<int>() { nums[minItemIndex], nums[leftIndex], nums[rightIndex] };
+                        var tempString = @$"{nums[minItemIndex]}&{nums[leftIndex]}&{nums[rightIndex]}";
+
+                        if (!uniqueResults.Contains(tempString))
+                        {
+                            results.Add(tempComb);
+                            uniqueResults.Add(tempString);
+                        }
+
+                        leftIndex++;
+                        rightIndex--;
+                        continue;
+                    }
+
+                    if (nums[minItemIndex] + nums[leftIndex] + nums[rightIndex] < 0)
+                    {
+                        leftIndex++;
+                        continue;
+                    }
+
+                    if (nums[minItemIndex] + nums[leftIndex] + nums[rightIndex] > 0)
+                    {
+                        rightIndex--;
+                        continue;
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public static IList<IList<int>> ThreeSumWithHashMap(int[] nums)
+        {
+            //var result = new List<IList<int>>();
+
+            //Array.Sort(nums);
+
+            //var map = nums.ToDictionary(x => x, x => Array.IndexOf(nums, x));
+
+            //for (int firstItemIndex = 0; firstItemIndex < nums.Length - 1; firstItemIndex++ )
+            //{
+            //    if (nums[firstItemIndex] > 0) break;
+            //    for (int secondItemIndex = firstItemIndex + 1; secondItemIndex < nums.Length; secondItemIndex++ )
+            //    {
+            //        int numsK = 0 - nums[i] - nums[j];
+            //    }
+            //}
+
+            return null;
+        }
+    }
+}
---
+++ topics/LeeCodeTest/0015-three-sum/ThreeSumImplTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0015_three_sum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0015_three_sum
+{
+    [TestClass()]
+    public class ThreeSumTests
+    {
+        [TestMethod()]
+        //[DataRow(new int[] { 0, 0, 0 })]
+        [DataRow(new int[] { -1, 0, 1, 2, -1, -4 })]
+        public void ThreeSumWithTwoPointersTest(int[] nums)
+        {
+            var results = ThreeSumImpl.ThreeSumWithTwoPointers(nums);
+
+            //CollectionAssert.AreEqual(expectedValues, results);
+        }
+    }
+}
