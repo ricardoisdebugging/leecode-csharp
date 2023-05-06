c5325f3d5d07286e8c8a3411727c772dedefa777
Ricardo <296060440@qq.com>
2023-05-06 21:49:05 +0800

    implement _0027_remove_elements

---
+++ topics/LeeCode/0027-remove-elements/RemoveElementsImpl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0027_remove_elements
+{
+    public class RemoveElementsImpl
+    {
+        public static int RemoveElementWithRefLinq(ref int[] nums, int val)
+        {
+            var cutList = nums.ToList().Where(x => x != val).ToList();
+            nums = cutList.ToArray();
+            return cutList.Count;
+        }
+
+        public static int RemoveElementWithLinq(int[] nums, int val)
+        {
+            var cutList = nums.ToList().Where(x => x != val).ToList();
+            for(int i = 0; i < cutList.Count; i++)
+                nums[i] = cutList[i];
+            return cutList.Count;
+        }
+
+        public static int RemoveElementWithPointer(int[] nums, int val)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                    nums[count++] = nums[i];
+            }
+            return count;
+        }
+    }
+}
---
+++ topics/LeeCodeTest/0027-remove-elements/RemoveElementsImplTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0027_remove_elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeeCode._0025_remove_duplicates_from_sort_array;
+
+namespace LeeCode._0027_remove_elements
+{
+    [TestClass()]
+    public class RemoveElementsImplTest
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 3, 2, 2, 3 }, 3, 2, new int[] { 2, 2 })]
+        [DataRow(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5, new int[] { 0, 1, 3, 0, 4 })]
+        public void RemoveElementWithRefLinqTest(int[] nums, int dropNum, int expectedNum, int[] expectedValues)
+        {
+            var k = RemoveElementsImpl.RemoveElementWithRefLinq(ref nums, dropNum);
+            Assert.AreEqual(expectedNum, k);
+
+            ValidateElementsInArray(nums, expectedValues, k);
+        }
+
+        [TestMethod()]
+        [DataRow(new int[] { 3, 2, 2, 3 }, 3, 2, new int[] { 2, 2 })]
+        [DataRow(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5, new int[] { 0, 1, 3, 0, 4 })]
+        public void RemoveElementWithLinqTest(int[] nums, int dropNum, int expectedNum, int[] expectedValues)
+        {
+            var k = RemoveElementsImpl.RemoveElementWithLinq(nums, dropNum);
+            Assert.AreEqual(expectedNum, k);
+
+            ValidateElementsInArray(nums, expectedValues, k);
+        }
+
+        [TestMethod()]
+        [DataRow(new int[] { 3, 2, 2, 3 }, 3, 2, new int[] { 2, 2 })]
+        [DataRow(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5, new int[] { 0, 1, 3, 0, 4 })]
+        public void RemoveElementWithPointerTest(int[] nums, int dropNum, int expectedNum, int[] expectedValues)
+        {
+            var k = RemoveElementsImpl.RemoveElementWithPointer(nums, dropNum);
+            Assert.AreEqual(expectedNum, k);
+
+            ValidateElementsInArray(nums, expectedValues, k);
+        }
+
+        private void ValidateElementsInArray(int[] nums, int[] expectedValues, int numCount)
+        {
+            for (int i = 0; i < numCount; i++)
+                Assert.AreEqual(nums[i], expectedValues[i]);
+        }
+    }
+}
