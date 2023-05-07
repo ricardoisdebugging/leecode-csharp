c3e42beeef3a37722f08f18116644227ee6eead7
Ricardo <296060440@qq.com>
2023-05-07 22:53:34 +0800

    implement _0031_next_permutation

---
+++ topics/LeeCodeTest/0031-next-permutation/NextPermutationTest.cs
@@ -0,0 +1,20 @@
+﻿namespace LeeCode._0031_next_permutation
+{
+    [TestClass()]
+    public class NextPermutationTest
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 1, 6, 5, 8, 4, 3, 2 }, new int[] { 1, 6, 2, 3, 4, 5, 8 })]
+        [DataRow(new int[] { 1, 5, 6, 8, 4, 3, 2 }, new int[] { 1, 5, 8, 2, 3, 4, 6 })]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 1, 3, 2 })]
+        [DataRow(new int[] { 1, 3, 2 }, new int[] { 2, 1, 3 })]
+        [DataRow(new int[] { 1, 1, 5 }, new int[] { 1, 5, 1 })]
+        public void NextPermutationSolution1Test(int[] nums, int[] nextArray)
+        {
+            NextPermutationImpl.NextPermutationSolution1(nums);
+
+            CollectionAssert.AreEquivalent(nextArray, nums);
+        }
+    }
+}
