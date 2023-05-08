0b64abacfa6a1a8b5a09c809fedb11f2e5d4990e
Ricardo <296060440@qq.com>
2023-05-08 23:37:56 +0800

    implement _0035_search_insert_position

---
+++ topics/LeeCode/0035-search-insert-position/SearchInsertPositionImpl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0035_search_insert_position
+{
+    public class SearchInsertPositionImpl
+    {
+        public static int SearchInsert(int[] nums, int target)
+        {
+            return BinarySearch(nums, target);
+        }
+
+        private static int BinarySearch(int[] nums, int target)
+        {
+            int leftIdx = 0;
+            int rightIdx = nums.Length - 1;
+            int midIdx = int.MaxValue;
+
+            int targetIdx = nums.Length;
+
+            while (leftIdx <= rightIdx)
+            {
+                midIdx = leftIdx + (rightIdx - leftIdx) / 2;
+
+                int mid = nums[midIdx];
+
+                if (target <= mid)
+                {
+                    targetIdx = midIdx;
+                    rightIdx = midIdx - 1;
+                }
+
+                if (target > mid)
+                    leftIdx = midIdx + 1;
+            }
+
+            return targetIdx;
+        }
+    }
+}
