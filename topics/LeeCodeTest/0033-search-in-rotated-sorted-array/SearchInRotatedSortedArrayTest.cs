ed0a5d81fa03f6320830b7ebff5857976f050377
Ricardo <296060440@qq.com>
2023-05-09 21:38:36 +0800

    implement _0033_search_in_rotated_sorted_array

---
+++ topics/LeeCodeTest/0033-search-in-rotated-sorted-array/SearchInRotatedSortedArrayTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0033_search_in_rotated_sorted_array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeeCode._0035_search_insert_position;
+
+namespace LeeCode._0033_search_in_rotated_sorted_array
+{
+    [TestClass()]
+    public class SearchInRotatedSortedArrayTest
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+        [DataRow(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+        [DataRow(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 8, -1)]
+        [DataRow(new int[] { 1 }, 0, -1)]
+        public void SearchTest(int[] nums, int target, int expectedInx)
+        {
+            var impl = new SearchInRotatedSortedArrayImpl();
+            var targetIdx = impl.Search(nums, target);
+
+            Assert.AreEqual(expectedInx, targetIdx);
+        }
+    }
+}
