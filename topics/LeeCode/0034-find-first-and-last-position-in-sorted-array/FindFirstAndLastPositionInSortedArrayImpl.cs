1cc366d283045a10b26ae606f5bfb08164715e36
Ricardo <296060440@qq.com>
2023-05-10 20:48:41 +0800

    implement  _0034_find_first_and_last_position_in_sorted_array

---
+++ topics/LeeCode/0034-find-first-and-last-position-in-sorted-array/FindFirstAndLastPositionInSortedArrayImpl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0034_find_first_and_last_position_in_sorted_array
+{
+    public class FindFirstAndLastPositionInSortedArrayImpl
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var firstIdx = BinarySearchForFirstElement(nums, target);
+            var lastIdx = BinarySearchForLastElement(nums, target);
+            return new int[] { firstIdx, lastIdx };
+        }
+
+        private static int BinarySearchForFirstElement(int[] nums, int target)
+        {
+            var leftIdx = 0;
+            var rightIdx = nums.Length - 1;
+
+            while (leftIdx <= rightIdx)
+            {
+                var midIdx = leftIdx + (rightIdx - leftIdx) / 2;
+
+                if (nums[midIdx] > target)
+                    rightIdx = midIdx - 1;
+
+                if (nums[midIdx] < target)
+                    leftIdx = midIdx + 1;
+
+                if (nums[midIdx] == target)
+                {
+                    //适用于递增数列
+                    if (midIdx == 0 || nums[midIdx - 1] != target)
+                        return midIdx;
+
+                    rightIdx = midIdx - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        private static int BinarySearchForLastElement(int[] nums, int target)
+        {
+            var leftIdx = 0;
+            var rightIdx = nums.Length - 1;
+
+            while (leftIdx <= rightIdx)
+            {
+                var midIdx = leftIdx + (rightIdx - leftIdx) / 2;
+
+                if (nums[midIdx] > target)
+                    rightIdx = midIdx - 1;
+
+                if (nums[midIdx] < target)
+                    leftIdx = midIdx + 1;
+
+                if (nums[midIdx] == target)
+                {
+                    //适用于递增数列
+                    if (midIdx == nums.Length - 1 || nums[midIdx + 1] != target)
+                        return midIdx;
+
+                    leftIdx = midIdx + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ topics/LeeCodeTest/0034-find-first-and-last-position-in-sorted-array/FindFirstAndLastPositionInSortedArrayTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0034_find_first_and_last_position_in_sorted_array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0034_find_first_and_last_position_in_sorted_array
+{
+    [TestClass()]
+    public class FindFirstAndLastPositionInSortedArrayTest
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 5, 7, 7, 8, 8, 10 }, 8, new int[] { 3, 4 })]
+        [DataRow(new int[] { 5, 7, 7, 8, 8, 10 }, 6, new int[] { -1, -1 })]
+        [DataRow(new int[] {  }, 0, new int[] { -1, -1 })]
+        public void SearchRangeTest(int[] nums, int target, int[] expectedRange)
+        {
+            var impl = new FindFirstAndLastPositionInSortedArrayImpl();
+            var ranges = impl.SearchRange(nums, target);
+
+            CollectionAssert.AreEqual(expectedRange, ranges);
+        }
+    }
+}
