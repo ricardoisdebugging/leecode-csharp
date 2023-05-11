3d10c0ddb9b25e5e13fd1d0e481d1103cc4f0533
Ricardo <ricardo-haitao.mao@cn.abb.com>
2023-05-11 16:34:19 +0800

    extract common funcs

---
+++ topics/LeeCodeTest/Common/Funcs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCodeTest.Common
+{
+    public class Funcs
+    {
+        public static IList<IList<int>> ConvertStringToTargetNestedList(string valueText)
+        {
+            var valuesList = valueText.Split("@@");
+            var result = new List<IList<int>>();
+            foreach (var values in valuesList)
+            {
+                var arr = values.Split('&').Select(x => int.Parse(x)).ToList();
+                result.Add(arr);
+            }
+
+            return result;
+        }
+    }
+}
