8c63e2774d0adca6f0834f79a0f256301fe408ee
Ricardo <296060440@qq.com>
2023-05-12 23:27:09 +0800

    implement _0088_merge_sorted_array using LINQ

---
+++ topics/LeeCodeTest/0088-merge-sorted-array/MergeSortedArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0088_merge_sorted_array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0088_merge_sorted_array
+{
+    [TestClass()]
+    public class MergeSortedArrayTest
+    {
+        [TestMethod()]
+        [DataRow(new int[] { -1, 0, 0, 3, 3, 3, 0, 0, 0 }, 6, new[] { 1, 2, 2 }, 3, new int[] { -1, 0, 0, 1, 2, 2, 3, 3, 3 })]
+        public void MergeTest(int[] nums1, int m, int[] nums2, int n, int[] expectedNums)
+        {
+            var mergeSortedArrayImpl = new MergeSortedArrayImpl();
+            mergeSortedArrayImpl.Merge(nums1, m, nums2, n);
+
+            CollectionAssert.Equals(mergeSortedArrayImpl, expectedNums);
+        }
+    }
+}
