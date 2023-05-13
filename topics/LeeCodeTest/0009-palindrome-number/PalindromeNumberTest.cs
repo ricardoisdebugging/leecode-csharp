338feea978fd4f6a50517acec5c13917146aeaae
Ricardo <296060440@qq.com>
2023-05-13 23:38:41 +0800

    implement _0009_palindrome_number with low performance

---
+++ topics/LeeCodeTest/0009-palindrome-number/PalindromeNumberTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0009_palindrome_number;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0009_palindrome_number
+{
+    [TestClass()]
+    public class PalindromeNumberTest
+    {
+        [TestMethod()]
+        [DataRow(121, true)]
+        [DataRow(-121, false)]
+        [DataRow(10, false)]
+        [DataRow(1, true)]
+        [DataRow(123321, true)]
+        public void IsPalindromeTest(int x, bool expectedValue)
+        {
+            var palindromeNumberImpl = new PalindromeNumberImpl();
+            var isParlindrom = palindromeNumberImpl.IsPalindrome(x);
+
+            Assert.AreEqual(expectedValue, isParlindrom);
+        }
+    }
+}
