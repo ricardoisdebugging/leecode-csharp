afc39d99979e7f4d062030cf21a077c7020ac7b3
Ricardo <296060440@qq.com>
2023-05-16 20:11:28 +0800

    implement _0061_rotate_list

---
+++ topics/LeeCodeTest/0061-rotate-list/RotateListTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0061_rotate_list;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeeCodeTest.Common;
+
+namespace LeeCode._0061_rotate_list
+{
+    [TestClass()]
+    public class RotateListTest
+    {
+        [TestMethod()]
+        //[DataRow(new int[] { 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 1, 2, 3 })]
+        //[DataRow(new int[] { 1, 2 }, 0, new int[] { 1, 2 })]
+        //[DataRow(new int[] { 1, 2 }, 1, new int[] { 2, 1 })]
+        //[DataRow(new int[] { 1, 2, 3 }, 2, new int[] { 2, 3, 1 })]
+        [DataRow(new int[] { 1 }, 1, new int[] { 1 })]
+        public void RotateRightTest(int[] originalList, int rotateIdx, int[] expectedList)
+        {
+            var impl = new RotateListImpl();
+
+            var origialNode = Funcs.ConvertArrayToListNode(originalList);
+            var realNode = impl.RotateRight(origialNode, rotateIdx);
+            var realList = Funcs.ConvertListNodeToList(realNode);
+
+            CollectionAssert.AreEqual(expectedList, realList);
+        }
+    }
+}
