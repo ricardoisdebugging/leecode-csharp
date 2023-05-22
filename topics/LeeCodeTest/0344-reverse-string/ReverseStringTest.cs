046aca65957d0cdd72ad637d98e6bfa2932bb990
Ricardo <296060440@qq.com>
2023-05-22 21:04:29 +0800

    implement _0344_reverse_string

---
+++ topics/LeeCodeTest/0344-reverse-string/ReverseStringTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeeCode._0344_reverse_string;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeCode._0344_reverse_string
+{
+    [TestClass()]
+    public class ReverseStringTest
+    {
+        [TestMethod()]
+        [DataRow("hannah", "hannah")]
+        [DataRow("hello", "olleh")]
+        [DataRow("i", "i")]
+        public void ReverseString(string value, string reverseValue)
+        {
+            char[] chars = value.ToCharArray();
+            char[] reverseChars = reverseValue.ToCharArray();
+
+            var impl = new ReverseStringImpl();
+            impl.ReverseString(chars);
+
+            CollectionAssert.AreEqual(chars, reverseChars);
+        }
+    }
+}
